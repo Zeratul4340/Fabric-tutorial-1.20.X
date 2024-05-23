fe74b6e676c6b2834f9889ff3d857ae0622970cb
Zeratul4340 <eduardoatroom37@gmail.com>
2024-05-23 01:16:37 -0700

    Blowing thy load

---
+++ src/main/java/net/zeratul4340/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.zeratul4340.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
