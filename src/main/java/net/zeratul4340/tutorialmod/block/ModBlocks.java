08a6e94288b83097d45a06ab59fa40cef42ab1a1
Zeratul4340 <eduardoatroom37@gmail.com>
2024-05-24 01:15:54 -0700

    custom block loot tables, tags, and ores implemented

--- src/main/java/net/zeratul4340/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/zeratul4340/tutorialmod/block/ModBlocks.java
@@ -3,12 +3,14 @@ package net.zeratul4340.tutorialmod.block;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.zeratul4340.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -16,6 +18,15 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
             new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.copy(Blocks.STONE).strength(2f)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(4, 10), AbstractBlock.Settings.copy(Blocks.DEEPSLATE).strength(4f)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(4, 10), AbstractBlock.Settings.copy(Blocks.NETHERRACK).strength(1.5f)));
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(8, 20), AbstractBlock.Settings.copy(Blocks.END_STONE).strength(4f)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
