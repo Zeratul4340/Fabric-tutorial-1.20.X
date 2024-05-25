44c3ce541629492f19a621aaf735c2c4c463d523
Zeratul4340 <eduardoatroom37@gmail.com>
2024-05-24 17:07:07 -0700

    self-imposed homework: improved metal detector to scan the entire way down on the selected Y-axis. Also changed to scan for metals only (Iron, Gold, Copper)

--- src/main/java/net/zeratul4340/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/zeratul4340/tutorialmod/item/custom/MetalDetectorItem.java
@@ -10,7 +10,13 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
+/**
+ * First ever implementation of a custom functional item
+ *
+ * I would add an author tag, but I followed a tutorial on this...
+ */
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
         super(settings);
@@ -27,26 +33,44 @@ public class MetalDetectorItem extends Item {
                 BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
 
                 if (i == 0 && isValuableBlock(state)) {
-                    player.sendMessage(Text.literal("It's right there..."));
-                    break;
+                    player.sendMessage(Text.literal("You're looking at an exposed metal ore..."));
+                    continue;
                 }
+                /*
+                Displays valuable metals.
+
+                Maybe for later I should make it scan tags instead of specific blocks,
+                and also just store the scanned ores in list. Also remove coords because they're moot.
+
+                Intent: "Found 2 Iron ores, 0 Copper ores, 1 Gold ores"
+                */
                 if(isValuableBlock(state)) {
                     outputValuableCoordinates(positionClicked.down(i), player, state.getBlock());
                     foundBlock = true;
-
+                }
+                /*
+                stop iterating on hitting bedrock or void (probably useless)
+                */
+                if(hitBedrock(state)) {
                     break;
                 }
             }
-
             if(!foundBlock) {
-                player.sendMessage(Text.literal("Nothing valuable found..."));
+                assert player != null;
+                player.sendMessage(Text.literal("No metals found..."));
             }
         }
-
+        /*
+          Note for later: This handles damage to the durability
+         */
         if (context.getPlayer() != null) {
             context.getStack().damage(1, context.getPlayer(), LivingEntity.getSlotForHand(context.getHand()));
         }
         return super.useOnBlock(context);
+    }
+
+    private boolean hitBedrock(BlockState state) {
+        return state.isOf(Blocks.BEDROCK) || state.isOf(Blocks.VOID_AIR);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
@@ -55,6 +79,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf((Blocks.DIAMOND_ORE));
+        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DEEPSLATE_IRON_ORE) || state.isOf(Blocks.COPPER_ORE) || state.isOf(Blocks.DEEPSLATE_COPPER_ORE) || state.isOf(Blocks.GOLD_ORE) || state.isOf(Blocks.DEEPSLATE_GOLD_ORE);
     }
 }
